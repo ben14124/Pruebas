9fd4672f1c6940b146887c0e145292e7f8c832a9
dbs_jd <dbs_jd@users-mbp>
2015-07-10 17:12:51 -0600

    Cambiaaaa plsssss

--- Pruebap/src/pruebap/Pruebap.java
+++ Pruebap/src/pruebap/Pruebap.java
@@ -15,7 +15,7 @@ public class Pruebap {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Funcionaaaaa por favorrr");
     }
     
 }
